f63289dfc7c48c50f7ae7d1e3187b67d7a912bee
ed-marquez <ed@swirldslabs.com>
2024-05-28 16:25:33 -0500

    Adding subscription and minor changes to index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.clear();
 
 require("dotenv").config();
 
-const { Client, AccountId, PrivateKey, TopicCreateTransaction, TopicMessageSubmitTransaction } = require("@hashgraph/sdk");
+const { Client, AccountId, PrivateKey, TopicCreateTransaction, TopicMessageQuery, TopicMessageSubmitTransaction } = require("@hashgraph/sdk");
 
 const network = process.env.NETWORK;
 const operatorId = AccountId.fromString(process.env.OPERATOR_ID);
@@ -17,16 +17,23 @@ async function main() {
 	console.log(`- Transaction status: ${newTopicReceipt.status.toString()} \n`);
 	console.log(`- New Topic ID: ${newTopicId.toString()} \n`);
 
+	// Wait 5 seconds between topic creation and subscription creation
+	await new Promise((resolve) => setTimeout(resolve, 5000));
+
+	// // Subscribe to the consensus topic
+	new TopicMessageQuery().setTopicId(newTopicId).subscribe(client, null, (message) => {
+		let messageAsString = Buffer.from(message.contents, "utf8").toString();
+		console.log(`${message.consensusTimestamp.toDate()} Received: ${messageAsString}`);
+	});
+
 	// // Submit 10 messages to the new topic using a for loop
 	for (let i = 0; i < 10; i++) {
-		const newMessage = `Hello, Hedera! #${i + 1}`;
+		const newMessage = `Hello Hedera! #${i + 1}`;
 		const newMessageTx = await new TopicMessageSubmitTransaction().setTopicId(newTopicId).setMessage(newMessage).execute(client);
 	}
 
 	// Check the topic messages on HashScan
 	const hashscanUrl = `https://hashscan.io/${network}`;
 	console.log(`- Check the topic messages on HashScan: \n ${hashscanUrl}/topic/${newTopicId.toString()} \n`);
-
-	client.close();
 }
 main();
